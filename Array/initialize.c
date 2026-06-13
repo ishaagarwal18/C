14bb25e80bf85b2480c3d210168a05f74e01fe83
ishaagarwal18 <ishaagarwal4688@gmail.com>
2026-06-13 18:24:22 +0530

    Array

---
+++ Array/initialize.c
@@ -0,0 +1,17 @@
+int ar[0]; // global  all values are zero
+int main()
+{
+    // int myarray[5]={1,2,3,3,5};
+    // int myarray[5]={1,2}; // initialize to 1,2,0,0,0
+    // int myarray[5]={0}; //all elements 0
+    // static int myarray[5]; //all elements 0
+    // int ar[0]; //locak  all values are garbage
+    // //if we want a[2]=1 and a[4]=4 :
+    // int a[10]={0,0,1,0,4} // remaining all are 0
+
+    char t[5]={'a','b','c','d'};
+    char s[5]="abcd";
+
+    // int mya[]={1,2,3,4,5,6,7,8,9}
+    char a[]="abcd"; 
+}
