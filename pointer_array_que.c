2d7b7a45994bdf5821d3025d12c4e5c602fd5e02
ishaagarwal18 <ishaagarwal4688@gmail.com>
2026-06-23 20:48:25 +0530

    Pointer array

--- pointer_array_que.c
+++ pointer_array_que.c
@@ -15,11 +15,13 @@ int main(void) {
     // printf("%d %d\n", a[0], *(p--));
     // printf("%d\n", p[0]);
 
-    int a1={1,2,4,8};
-    int *b=a+2;
+    int a1[]={ 1, 2, 4, 8};
+    int *b=a1+2;
     int *c=b--+1;
     printf("%d\n",*b);
     printf("%d\n",*c);
+
+    printf("%d\n",a1[-1]);
 
     return 0;
 }
