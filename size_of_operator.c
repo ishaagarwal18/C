6137b5a4fd36aea138dd2dffc8c7679bcafb55f3
ishaagarwal18 <ishaagarwal4688@gmail.com>
2026-06-23 19:20:02 +0530

    C language

---
+++ size_of_operator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+//     int i=1;
+//     printf("%d\n",sizeof(i++));
+//     printf("%d\n",i);
+
+//     int a[5];
+//     printf("%d\n",a);
+//     printf("%d\n",(a+1));
+//     printf("%d\n",(a+2));
+//     printf("%d\n",(a+3));
+
+//     int b;
+//     long int c;
+//     printf("%d\n",sizeof(b+c));
+
+
+    // int *j;
+    // printf("%d\n",sizeof(j));
+
+
+    int a[10];
+    int *p;
+
+    p=a;
+    printf("%d\n",sizeof(p));
+    printf("%d\n",sizeof(a));
+    
+}
